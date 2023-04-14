fbf5c9d82f9ae4b17f14064c806f53a415b30e2e
ivansa-ru <ivansa@me.com>
2023-04-14 11:02:13 +0300

    Build wf (#1)
    * add workflow
    
    * trigger
    
    * trigger
    
    * trigger build
    
    * provide version
    
    * trigger
    
    * add psycopg2
    
    * trigger build
    
    * remove legacy orm & smb
    
    * workflow branch
    
    * long desc
    
    * prerelease
    
    * trigger
    
    * trigger
    
    * on push
    
    * remove vim

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup
 
-__version="0.0.1"
+__version="1.0.0"
 
 spec = {
     "name": "oc_connections",
     "version": __version,
     "license": "Apache License 2.0",
     "description": "Connection Manages",
+    "long_description": "",
+    "long_description_content_type": "text/plain",
     "packages": ["oc_connections"],
     "install_requires": [
       'oc_cdtapi', 
